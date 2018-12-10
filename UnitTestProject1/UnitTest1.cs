a761f8b3cedd2d399dc22adb5b97fb46728bc307
gabrielmt <gabrielmtararam@gmail.com>
2018-12-10 09:15:04 -0200

    first Test with winium

--- UnitTestProject1/UnitTest1.cs
+++ UnitTestProject1/UnitTest1.cs
@@ -1,14 +1,22 @@
 ﻿using System;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
+using OpenQA.Selenium.Remote;
+using OpenQA.Selenium.Winium;
 
 namespace UnitTestProject1
 {
     [TestClass]
     public class UnitTest1
     {
+
+
         [TestMethod]
-        public void TestMethod1()
+        public void TestNotePad()
         {
+            DesktopOptions options = new DesktopOptions();
+            options.ApplicationPath = @"C:\Windows\notepad.exe";
+            WiniumDriver driver = new WiniumDriver(@"C:\Users\Eletronica10\source\repos\UnitTestProject1\UnitTestProject1",options);//Winium.Desktop.Driver.exe
+            driver.FindElementByClassName("Edit").SendKeys("hello world");
         }
     }
 }
